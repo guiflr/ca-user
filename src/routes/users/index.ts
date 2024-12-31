f07fb14bf1f2a0531ff7e9ad6ee41ecf24ba14cc
guilherme <guiflr97@gmail.com>
2024-12-31 17:29:39 -0300

    feat: add get user route by id

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { expressAdapter } from '../../adapters/ExpressAdapter'
 import { makeCreateUserController } from '../../factory/CreateUserFactory'
+import { makeGetUserController } from '../../factory/GetUserFactory'
 const userRoutes = Router()
 
 userRoutes.post('/users', expressAdapter(makeCreateUserController()))
+userRoutes.get('/users/:id', expressAdapter(makeGetUserController()))
 
 export default userRoutes
