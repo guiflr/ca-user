8ee1826b7da303fa6473730d7a44bee754c19a33
guilherme <guiflr97@gmail.com>
2024-12-31 17:28:29 -0300

    chore: get tests from src root

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  rootDir: "./src/tests",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
